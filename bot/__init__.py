9f10a8f568b1295d9e2dd0b89d82e69cdd68c3e1
Arsalan <126089283+a092ars3@users.noreply.github.com>
2023-03-06 12:13:07 +0530

    test

--- bot/__init__.py
+++ bot/__init__.py
@@ -356,9 +356,8 @@ if ospath.exists('list_drives.txt'):
             else:
                 INDEX_URLS.append('')
 
-if BASE_URL:
-    Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{SERVER_PORT}", shell=True)
-
+PORT = environ.get('PORT')
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}", shell=True)
 srun(["qbittorrent-nox", "-d", f"--profile={getcwd()}"])
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
